99a8f4ee284ef6b8269b26c9288089143db6fe3c
waqas-ahmd <65waqas@gmail.com>
2022-08-16 14:23:38 +0500

    init

---
+++ stories/Thing.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Thing, Props } from '../src';
+
+const meta: Meta = {
+  title: 'Welcome',
+  component: Thing,
+  argTypes: {
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: Story<Props> = args => <Thing {...args} />;
+export const Default = Template.bind({});
+
+Default.args = {};
